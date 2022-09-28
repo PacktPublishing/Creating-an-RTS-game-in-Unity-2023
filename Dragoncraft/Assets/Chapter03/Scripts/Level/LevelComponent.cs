b2ad35619ef7b1eafe2849ba082bcf380792f6eb
Bruno Cicanci <brunocicanci@gmail.com>
2022-09-28 22:18:01 +0100

    Added the level component

---
+++ Dragoncraft/Assets/Chapter03/Scripts/Level/LevelComponent.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+namespace Dragoncraft
+{
+    public class LevelComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private LevelData _levelData;
+
+        [SerializeField]
+        private GameObject _plane;
+
+        private void Start()
+        {
+            if (_levelData == null || _plane == null)
+            {
+                Debug.LogError("Missing LevelData or Plane");
+                return;
+            }
+
+            Collider planeCollider = _plane.GetComponent<Collider>();
+
+            Vector3 planeSize = planeCollider.bounds.size;
+
+            Vector3 startPosition = new Vector3(-planeSize.x / 2, 0, planeSize.z / 2);
+
+            float offsetX = planeSize.x / _levelData.Columns - 1;
+
+            float offsetZ = planeSize.z / _levelData.Rows - 1;
+
+            InitializeLevel(startPosition, offsetX, offsetZ);
+        }
+
+        private void InitializeLevel(Vector3 startPosition, float offsetX, float offsetZ)
+        {
+            foreach (LevelSlot slot in _levelData.Slots)
+            {
+                LevelItem levelItem = _levelData.Configuration.FindByType(slot.ItemType);
+                if (levelItem == null)
+                {
+                    continue;
+                }
+
+                float x = startPosition.x + (slot.Coordinates.y * offsetX) + offsetX / 2;
+                float z = startPosition.z - (slot.Coordinates.x * offsetZ) - offsetZ / 2;
+                Vector3 position = new Vector3(x, 0, z);
+
+                Instantiate(levelItem.Prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
